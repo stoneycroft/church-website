448882b658d452793d1f396d94ca3b2af8aca3be
Peter Prescott <prescott.peter@gmail.com>
2021-05-24 19:44:16 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'The Stoneycroft Corps',
+  siteName: 'The Liverpool Stoneycroft Corps',
   siteDescription: 'A Salvation Army church.',
 
   templates: {
